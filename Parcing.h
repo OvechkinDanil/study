7023df019e7bc824559e2896b46778cfd4bec869
OvechkinDanil <47829271+OvechkinDanil@users.noreply.github.com>
2020-05-18 11:23:27 +0300

    Add files via upload

---
+++ Parcing.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Classes.h"
+
+void split(const string& str, vector<string>& tokens, const string& delimiters = " ");
+
+void TextParcing(string filename);
---
+++ Source.cpp
@@ -0,0 +1,27 @@
+﻿#include "Rendering.h"
+#include "glut.h"
+Menu menu;
+int init = 0;
+int deltaX = 60;
+int deltaY = 50;
+int WinW = 640;
+int WinH = 480;
+list<MenuItem> curItems;
+
+int main(int argc, char** argv)
+{
+	TextParcing("test123.txt");
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 100);
+	glutInitWindowSize(WinW, WinH);
+	glutCreateWindow("Test");
+
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(render);
+	glutTimerFunc(33, Timer, 0);
+	
+	glutMouseFunc(MousePressed);
+
+	glutMainLoop();
+}
