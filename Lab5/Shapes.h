d0af096bd9907f1099195029855cd19fee5115ba
OvechkinDanil <47829271+OvechkinDanil@users.noreply.github.com>
2020-06-10 13:31:52 +0300

    Create Shapes.h

---
+++ Lab5/Shapes.h
@@ -0,0 +1,17 @@
+#include "Eraser.h"
+#include "Pencil.h"
+
+
+void drawPoint(int x, int y);
+
+void drawLine(int x0, int y0, int x1, int y1);
+
+float getRadius(int initX, int initY, int x, int y);
+
+float cMXSX(float x);
+
+float cMYSY(float y);
+
+void drawRectangle(int x1, int y1, int x0, int y0);
+
+void drawCircle(int toX, int toY);
