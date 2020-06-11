1eec317d7a9339ac0eeadba8b135029985ece646
OvechkinDanil <47829271+OvechkinDanil@users.noreply.github.com>
2020-06-11 11:59:18 +0300

    Update Pencil.h

--- Lab5/Pencil.h
+++ Lab5/Pencil.h
@@ -3,12 +3,19 @@
 
 class Pencil : public Brush
 {
+	private:
+		color_t color;
 	public:
 		Pencil()
 		{
 		}
 		Pencil(size_items_t n_size, color_t n_color)
-			:Brush(n_size, n_color)
+			:Brush(n_size)
 		{
+			color = n_color;
+		}
+		const void SetColor(color_t n_color)
+		{
+			color = n_color;
 		}
 };
